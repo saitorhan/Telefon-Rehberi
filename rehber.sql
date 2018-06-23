d05959361f5c87df06572b1edae10101a7e1b4b4
Sait ORHAN <saitorhan@outlook.com>
2018-06-23 10:23:39 +0300

    Initial commit

---
+++ rehber.sql
@@ -0,0 +1,11 @@
+INSERT INTO `rehber` VALUES ('Öznur ', 'Turan', 'Yeni mah. Demir sok. no:5/3 KARS MERKEZ', '0864567823', 'oznur@hotmail.com');
+INSERT INTO `rehber` VALUES ('halime', 'EKİN', 'gül mah.  sedef sok. no:7 MUŞ ', '0789234566', 'halime@hotmail.com');
+INSERT INTO `rehber` VALUES ('Sait ', 'Mustafa', 'a mah. b sok. no:4 BATMAN', '05432584512', 'sait@gmail.com');
+INSERT INTO `rehber` VALUES ('Sıla', 'Fidan', 'DENİZ MAH. DEFNE SOK. NO:1 KASTAMONU KOZAKLI', '3456782345', 'sila@hotmail.com');
+INSERT INTO `rehber` VALUES ('saitnur', 'yağmağan', 'cccccccccccccc mah. aaaaaaaaaaa sok. Batman', '1234567890', 's@hotmail.com');
+INSERT INTO `rehber` VALUES ('ayşe', 'hilmet', 'dddddddddddddddd mah. ssssssssssssssssss sok. no:6 İZMİR', '4567897654', 'ayse@hotmail.com');
+INSERT INTO `rehber` VALUES ('gaye', 'aksu', 'kkkkkkkkkkkkk mah. fffffffffffffff sok. no:9 ANKARA', '8765433221', 'gaye@gmail.com');
+INSERT INTO `rehber` VALUES ('oya', 'bak', 'HHHHHHHHHHH sok. bbbbbbbbbb mah. DENİZLİ', '8767654565', 'oya@gmail.com');
+INSERT INTO `rehber` VALUES ('Mehmed', 'Cuma', '?lçe?l', '1234345324', '');
+INSERT INTO `rehber` VALUES ('Bilal', 'Babayi?it', '?lçe?l', '1234567890', 'aaaaa@ddd.com');
+INSERT INTO `rehber` VALUES ('Bilal', 'baba', '?lçe?l', '1234567890', '');
