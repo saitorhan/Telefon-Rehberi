41c3276d1e6362a9e4fa2c6a28300af8e327e5fc
Sait ORHAN <saitorhan@outlook.com>
2018-07-03 09:04:29 +0300

    Update rehber.sql

--- rehber.sql
+++ rehber.sql
@@ -1,8 +1,8 @@
-INSERT INTO `rehber` VALUES ('Öznur ', 'Turan', 'Yeni mah. Demir sok. no:5/3 KARS MERKEZ', '0864567823', 'oznur@hotmail.com');
-INSERT INTO `rehber` VALUES ('halime', 'EKİN', 'gül mah.  sedef sok. no:7 MUŞ ', '0789234566', 'halime@hotmail.com');
-INSERT INTO `rehber` VALUES ('Sait ', 'Mustafa', 'a mah. b sok. no:4 BATMAN', '05432584512', 'sait@gmail.com');
-INSERT INTO `rehber` VALUES ('Sıla', 'Fidan', 'DENİZ MAH. DEFNE SOK. NO:1 KASTAMONU KOZAKLI', '3456782345', 'sila@hotmail.com');
-INSERT INTO `rehber` VALUES ('saitnur', 'yağmağan', 'cccccccccccccc mah. aaaaaaaaaaa sok. Batman', '1234567890', 's@hotmail.com');
+INSERT INTO `rehber` VALUES ('Sait ', 'ORHAN', 'Yeni mah. Demir sok. no:23/3 KOCASİNAN MERKEZ', '0864567823', 'sait@hotmail.com');
+INSERT INTO `rehber` VALUES ('Kasım', 'EKİN', 'gül mah.  sedef sok. no:7 MUŞ ', '0789234566', 'kasim@hotmail.com');
+INSERT INTO `rehber` VALUES ('Sait ', 'Mustafa', 'a mah. b sok. no:4 BATMAN', '05432438362', 'sait@gmail.com');
+INSERT INTO `rehber` VALUES ('SERDAR', 'Fidan', 'DENİZ MAH. DEFNE SOK. NO:1 KASTAMONU KOZAKLI', '3456782345', 'sila@hotmail.com');
+INSERT INTO `rehber` VALUES ('SADIK', 'yağmağan', 'cccccccccccccc mah. aaaaaaaaaaa sok. Batman', '1234567890', 's@hotmail.com');
 INSERT INTO `rehber` VALUES ('ayşe', 'hilmet', 'dddddddddddddddd mah. ssssssssssssssssss sok. no:6 İZMİR', '4567897654', 'ayse@hotmail.com');
 INSERT INTO `rehber` VALUES ('gaye', 'aksu', 'kkkkkkkkkkkkk mah. fffffffffffffff sok. no:9 ANKARA', '8765433221', 'gaye@gmail.com');
 INSERT INTO `rehber` VALUES ('oya', 'bak', 'HHHHHHHHHHH sok. bbbbbbbbbb mah. DENİZLİ', '8767654565', 'oya@gmail.com');
